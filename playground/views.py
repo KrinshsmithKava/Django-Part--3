d2489617638e46e7b202e039ac9d2a2ee48ea282
KrinshsmithKava <krinshsmith.nivzen@gmail.com>
2026-04-15 15:12:09 +0530

    django

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,14 @@
-from email import message
-from django.core.mail import EmailMessage, BadHeaderError
+from django.core.cache import cache
 from django.shortcuts import render
-from templated_mail.mail import BaseEmailMessage
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
+import requests
 
-def say_hello(request):
-    try:
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={'name': 'Mosh'}
-        )
-        message.send(['john@moshbuy.com'])
-    except BadHeaderError:
-        pass
-    return render(request, 'hello.html', {'name': 'Mosh'})
+
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        return render(request, 'hello.html', {'name': 'Mosh'})
